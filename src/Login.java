3bed2fb1c8122e407bf5e25a567149062008f661
Islam Erlan-uulu <131774656+islamerlanuulu@users.noreply.github.com>
2025-06-20 12:00:48 +0600

    lesson 5

---
+++ src/Login.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Login {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final String LOGIN = "admin";
+        final String PASSWORD = "123";
+
+        String userLogin;  String userPassword;
+
+        int totalCount = 0;
+
+        while (true) {
+            System.out.println("Enter your login: ");
+            userLogin =  scanner.nextLine();
+            System.out.println("Enter your password: ");
+            userPassword = scanner.nextLine();
+
+            if (userLogin.equalsIgnoreCase(LOGIN) && userPassword.equals(PASSWORD)) {
+                System.out.println("You are logged in!");
+                System.out.println("Количество попыток; " + ++totalCount);
+                break;
+            } else  {
+                System.out.println("Wrong password or login!");
+                System.out.println("Количество попыток; " + ++totalCount);
+            }
+        }
+    }
+}
---
+++ src/Loop.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("1. Вывести на экран 10 раз своё имя");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i+1 + " Ислам");
+        }
+
+        System.out.println("2. Вывести на экран числа от 1 до 100");
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i+1);
+        }
+
+        System.out.println("3. Вывести на экран все четные числа от 1 до 100");
+        for (int i = 2; i <= 100; i+=2) {
+            System.out.println(i);
+        }
+
+        System.out.println("4. Вывести на экран все числа от 1 до 100 кратные 4");
+        for (int i = 0; i < 100; i++) {
+            if (i % 4 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
